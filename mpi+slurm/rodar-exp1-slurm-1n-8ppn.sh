86400fb99f8dd185233eca3dc5e67aeec1d11b89
vitor <vitorcumim@gmail.com>
2024-12-12 17:08:25 -0300

    coloquei os exemplos do professor

---
+++ mpi+slurm/rodar-exp1-slurm-1n-8ppn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --exclusive
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --job-name=mpihello_job
+echo -----------------------------------
+echo -----------------------------------
+echo -----------------------------------
+echo ---------- ATENCAO: usar --exclusive na linha de comando
+echo -----------------------------------
+
+##### SBATCH --output=saida.out
+
+echo "$0 rodando no host " `hostname`  
+echo "$0 rodando no host " `hostname` >saida.txt
+
+echo "SLURM_JOB_NAME: "	$SLURM_JOB_NAME	
+echo "SLURM_NODELIST: " $SLURM_NODELIST 
+echo "SLURM_JOB_NODELIST: " $SLURM_JOB_NODELIST
+echo "SLURM_JOB_CPUS_PER_NODE: " $SLURM_JOB_CPUS_PER_NODE
+
+# Load necessary modules (adjust paths as needed)
+#module load openmpi/4.1.4
+
+# Run the MPI program
+mpirun ./mpiHello
+
+echo "O tempo total dessa shell foi de" $SECONDS "segundos"
+echo "SLURM_JOB_NAME: "	$SLURM_JOB_NAME	
+echo "SLURM_NODELIST: " $SLURM_NODELIST 
+echo "SLURM_JOB_NODELIST: " $SLURM_JOB_NODELIST
+echo "SLURM_JOB_CPUS_PER_NODE: " $SLURM_JOB_CPUS_PER_NODE
+#imprime infos do job slurm (e.g. TEMPO até aqui no fim do job)
+squeue -j $SLURM_JOBID

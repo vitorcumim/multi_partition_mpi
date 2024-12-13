cc12c3d29e705d93d91d9ab3b606d9384938cb40
Rafael Urbanek Laurentino <rafaurbanek@rafael_urbanek@hotmail.com>
2024-12-12 23:17:37 -0300

    testes finais

--- rodar-exp2-slurm-2n-8ppn.sh
+++ rodar-exp2-slurm-2n-8ppn.sh
@@ -8,7 +8,7 @@ echo -----------------------------------
 echo -----------------------------------
 echo ---------- ATENCAO: usar --exclusive na linha de comando
 echo -----------------------------------
-
+echo "USAGE: ./rodar-exp1-slurm-1n-8ppn.sh <nElements>"
 ##### SBATCH --output=saida.out
 
 echo "$0 rodando no host " `hostname`  
@@ -19,11 +19,23 @@ echo "SLURM_NODELIST: " $SLURM_NODELIST
 echo "SLURM_JOB_NODELIST: " $SLURM_JOB_NODELIST
 echo "SLURM_JOB_CPUS_PER_NODE: " $SLURM_JOB_CPUS_PER_NODE
 
+NTIMES=10
+echo "nt " $NTIMES
+MAX_EXECS=10
+echo "MAX_EXECS " $MAX_EXECS
+
+echo "Executando $NTIMES vezes com $1 elementos:"
+for i in $(seq 1 $NTIMES);
+do
+    echo "-----------------------------" >>saida.txt
+    if [ $i -le $MAX_EXECS ];
+    then 
+        mpirun ./particiona $1 | tee -a saida.txt | grep -oP '(?<=total_time_in_seconds: )[^ ]*' 
+    fi  
+done
+
 # Load necessary modules (adjust paths as needed)
 #module load openmpi/4.1.4
-
-# Run the MPI program
-mpirun ./mpiHello
 
 echo "O tempo total dessa shell foi de" $SECONDS "segundos"
 echo "SLURM_JOB_NAME: "	$SLURM_JOB_NAME	
